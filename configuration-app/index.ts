56495dbaf8d065a404f169106fae9afde890835b
Guy Marriott <guy@chec.io>
2021-11-23 19:21:56 -0800

    Update missing docs and use latest SDKs & typescript functionality

--- configuration-app/index.ts
+++ configuration-app/index.ts
@@ -1,7 +1,8 @@
 import { createSDK } from '@chec/integration-configuration-sdk';
+import { ConfigurationType } from '../configuration-type';
 
 (async () => {
-  const sdk = await createSDK();
+  const sdk = await createSDK<ConfigurationType>();
 
   // Refer to the configuration SDK docs to provide advice on building your custom configuration app:
   // https://github.com/chec/integration-configuration-sdk
